87a26e4396cc2ea2a06a7fa4ae287d1118ea7c87
Vladimir <vromanyuk@it7.com.ua>
2016-01-08 00:21:57 +0200

    initial commit, movement x

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float inputDirection;
+	private Vector3 moveVector;
+	private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputDirection = Input.GetAxis("Horizontal");
+		moveVector = new Vector3 (inputDirection, 0, 0);
+		controller.Move (moveVector);
+	}
+}
