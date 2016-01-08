dd1b81f3ecb1ed1ec9f607a0b5e716eea6b29869
vherever <vherever@gmail.com>
2016-01-09 01:09:51 +0200

    if player is grounded

--- Assets/__Scripts/Player.cs
+++ Assets/__Scripts/Player.cs
@@ -22,10 +22,11 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		IsControllerGrounded ();
 		moveVector = Vector3.zero;
 		inputDirection = Input.GetAxis("Horizontal") * speed;
 
-		if (controller.isGrounded) {
+		if (IsControllerGrounded()) {
 			verticalVelocity = 0;
 
 			if (Input.GetKeyDown (KeyCode.Space)) {
@@ -51,12 +52,34 @@ public class Player : MonoBehaviour {
 		lastMotion = moveVector;
 	}
 
+	private bool IsControllerGrounded() {
+		Vector3 leftRayStart;
+		Vector3 rightRayStart;
+
+		leftRayStart = controller.bounds.center;
+		rightRayStart = controller.bounds.center;
+
+		leftRayStart.x -= controller.bounds.extents.x;
+		rightRayStart.x += controller.bounds.extents.x;
+
+		Debug.DrawRay (leftRayStart, Vector3.down, Color.red);
+		Debug.DrawRay (rightRayStart, Vector3.down, Color.blue);
+
+		if(Physics.Raycast (leftRayStart, Vector3.down, (controller.height / 2) + 0.2f))
+			return true;
+
+		if(Physics.Raycast (rightRayStart, Vector3.down, (controller.height / 2) + 0.2f))
+			return true;
+
+		return false;
+	}
+
 	private void OnControllerColliderHit(ControllerColliderHit hit) {
 		if (controller.collisionFlags == CollisionFlags.Sides) {
 			if(Input.GetKeyDown(KeyCode.Space)) {
 				Debug.DrawRay(hit.point, hit.normal, Color.red, 2.0f);
 				moveVector = hit.normal * speed;
-				moveVector.y = jumpForce;
+				verticalVelocity = jumpForce;
 				secondJumpAvail = true;
 			}
 
