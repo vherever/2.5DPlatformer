3a95374462e65903ec2df1ace7995dd47fd1387c
vherever <vherever@gmail.com>
2016-01-08 13:30:02 +0200

    movement

--- Assets/__Scripts/Player.cs
+++ Assets/__Scripts/Player.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	private float inputDirection;
+	private float inputDirection; // X
+	private float verticalVelocity; // Y
+
+	private float speed = 5.0f;
+	private float gravity = 1.0f;
+
 	private Vector3 moveVector;
 	private CharacterController controller;
+
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController> ();
@@ -13,8 +19,15 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		inputDirection = Input.GetAxis("Horizontal");
-		moveVector = new Vector3 (inputDirection, 0, 0);
-		controller.Move (moveVector);
+		inputDirection = Input.GetAxis("Horizontal") * speed;
+
+		if (controller.isGrounded) {
+			verticalVelocity = 0;
+		} else {
+			verticalVelocity -= gravity;
+		}
+
+		moveVector = new Vector3 (inputDirection, verticalVelocity, 0);
+		controller.Move (moveVector * Time.deltaTime);
 	}
 }
