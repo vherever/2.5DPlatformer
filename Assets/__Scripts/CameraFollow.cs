f9475bd45c915c90db851ad30670e7998eeba41f
vherever <vherever@gmail.com>
2016-01-09 13:05:46 +0200

    camera follow

---
+++ Assets/__Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform lookAt;
+	private Vector3 offset = new Vector3(0.0f, 1.0f, -8.0f);
+	// Use this for initialization
+	private void Start () {
+
+	}
+
+	private void LateUpdate() {
+		transform.position = lookAt.transform.position + offset;
+	}
+
+}
